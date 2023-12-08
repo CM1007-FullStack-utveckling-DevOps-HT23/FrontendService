4d69771f073593a343ded71ba9d38e950fd354ec
Devve00 <granath.devve@gmail.com>
2023-12-08 14:17:26 +0100

    Update config.jsx

--- patient_frontend/src/config.jsx
+++ patient_frontend/src/config.jsx
@@ -1,3 +1,4 @@
-export const API_USER = "http://user-service:8080/user"
-export const API_PATIENT = "http://patient-service:8080/patient"
-export const API_MESSAGE = "http://message-service:8080/message"
+export const API_USER = `http://localhost:8081/user`
+export const API_PATIENT = `http://localhost:8082/patient`
+export const API_MESSAGE = `http://localhost:8080/message`
+export const API_QUARKUS_SEARCH = `http://localhost:8083/getPatients`
