4c2f3c05347b451c3d558ef16e5cae46ab65582d
Robin Flink <robinflink35@gmail.com>
2024-01-06 18:35:53 +0100

    Config change

--- patient_frontend/src/config.jsx
+++ patient_frontend/src/config.jsx
@@ -1,5 +1,15 @@
+/*
+//Locally
 export const API_USER = `http://localhost:8081/user`
 export const API_PATIENT = `http://localhost:8082/patient`
 export const API_MESSAGE = `http://localhost:8080/message`
 export const API_QUARKUS_SEARCH = `http://localhost:8083`
 export const API_IMAGE = `http://localhost:8084`
+*/
+
+//CBH Cloud
+export const API_USER = `https://user-service-dgrf2.app.cloud.cbh.kth.se/user`
+export const API_PATIENT = `https://patient-service-dgrf.app.cloud.cbh.kth.se/patient`
+export const API_MESSAGE = `https://message-service-dgrf.app.cloud.cbh.kth.se/message`
+export const API_QUARKUS_SEARCH = `https://search-service-dgrf.app.cloud.cbh.kth.se`
+export const API_IMAGE = `https://image-service-dgrf.app.cloud.cbh.kth.se`
