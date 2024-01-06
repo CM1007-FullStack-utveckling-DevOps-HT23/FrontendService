acd4db53b67e952434ebb3faf19507feccbc47ad
Robin Flink <robinflink35@gmail.com>
2024-01-06 18:19:08 +0100

    Ändrade keycloak.

--- patient_frontend/src/BackendScripts/Keycloak.js
+++ patient_frontend/src/BackendScripts/Keycloak.js
@@ -2,8 +2,20 @@
 
 import Keycloak from "keycloak-js";
 const keycloak = new Keycloak({
+    url: "https://130.237.11.66:2530/",
+    realm: "fullstack_cm1007",
+    clientId: "React-auth-Journal",
+});
+export default keycloak;
+
+
+/*
+import Keycloak from "keycloak-js";
+const keycloak = new Keycloak({
     url: "http://localhost:8080/auth",              //Change this to the correct address
     realm: "Journal-realm",                         //Change this to the name of the realm
     clientId: "React-auth-Journal",                 //Change this to the name of the created client. OBS: DON'T FORGET MAPPING!!!
 });
 export default keycloak;
+
+ */
